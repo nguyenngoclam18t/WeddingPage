6d34f16101932552f1c7e627f6b5511686540bfe
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2023-05-13 17:17:13 +0700

    Add files via upload

---
+++ HomePage/Assets/js/app.js
@@ -0,0 +1,36 @@
+var listImg=["./HomePage/Assets/image/banner-img1-1860x940_1506c8f432bc8142adcf778620ec2596.jpg","./HomePage/Assets/image/photo-1620315118895-4f3ed831d14d.webp"];
+var currentIndex=1;
+var showHeader= document.querySelector(".containers");
+function slideShows(i){
+    var slide= document.querySelector(".slide-show"); 
+   slide.style.backgroundImage=`linear-gradient(rgba(0, 0, 0, 0.2),
+   rgba(0, 0, 0, 0.2)), url(${listImg[i]})`;
+}
+window.onscroll=function(){
+    var rectHeader=showHeader.getClientRects()[0];
+
+    if(rectHeader.y!=0){
+        document.querySelector("#head").classList.add("turnOnHeader");
+    }else{
+        document.querySelector("#head").classList.remove("turnOnHeader");
+    }
+
+}
+
+setInterval(()=>{
+    if(currentIndex==listImg.length)currentIndex=0;
+    slideShows(currentIndex);
+    currentIndex++;
+},5000);
+function mouseOverNav(i) {
+    document.querySelectorAll(".nav-item").forEach((element,index)=>{
+        if(index==i){element.classList.add("underScore")}
+
+    })  }
+  
+  function mouseOutNav(i) {
+    document.querySelectorAll(".nav-item").forEach((element,index)=>{
+        if(index==i){element.classList.remove("underScore")}
+    })
+
+  }
